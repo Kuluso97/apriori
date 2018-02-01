40939abc6c92074c8a776bb4334b654fbae07b9a
Jason Wang <ycwanglovecake@gmail.com>
2018-02-01 02:21:40 -0500

    First successful implementation

--- apriori.py
+++ apriori.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from collections import defaultdict
 
 def getTransactionListAndItemSet(dataSet):
@@ -7,31 +8,51 @@ def getTransactionListAndItemSet(dataSet):
 	itemDict = defaultdict(int)
 
 	for line in inputFile:
-		transaction = set(line.split())
+		transaction = frozenset(line.split())
 		transactionList.append(transaction)
 		for item in transaction:
 			itemDict[item] += 1
 
 	return transactionList, itemDict
 
-def getCandidateSet(freqSet):
-	
+def getCandidateSet(freqSet, length):
+	return set([i.union(j) for i in freqSet for j in freqSet if len(i.union(j)) == length])
+
+def printAnswer(resultList):
+	for item in resultList:
+		s = ""
+		for i in item[0]:
+			s += i + ' '
+		s += '({0})'.format(item[1])
+		print(s)
 
 def main():
+	t0 = time.time()
 	dataSet, ms = sys.argv[1:]
 	ms = int(ms)
-	resultSet = set()
 
 	transactionList, itemDict = getTransactionListAndItemSet(dataSet)
-	freqSet = set(frozenset(i) for i in itemDict if itemDict[i] >= ms)
-	resultSet = resultSet.union(freqSet)
+	freqSet = set(frozenset([i]) for i in itemDict if itemDict[i] >= ms)
+	resultList = [(frozenset([i]),j) for i, j in itemDict.items() if j >= ms]
 	
+	length = 2
+
 	while freqSet:
-		CandidateSet = getCandidateSet(freqSet)
+		candidateSet = getCandidateSet(freqSet, length)
+		localDict = defaultdict(int)
+		for transaction in transactionList:
+			for candidate in candidateSet:
+				if candidate < transaction:
+					localDict[candidate] += 1
 
+		freqSet = set(frozenset(i) for i in localDict if localDict[i] >= ms)
+		resultList += [(i,j) for i, j in localDict.items() if j >= ms]
+		length += 1
 
+	printAnswer(resultList)
 
-
+	t1 = time.time()
+	print(t1-t0)
 
 if __name__ == '__main__':
 	main()
